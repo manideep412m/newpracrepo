75d4313250a65f54adab4ab2f9692d44b3cf058e
manideep412m <manideep412m@gmail.com>
2024-11-21 00:39:37 +0000

    vsc

--- main.tf
+++ main.tf
@@ -1,1 +1,11 @@
 
+provider "azurerm"{
+  features {}
+
+  subscription_id = "97d62e53-6132-48c5-a9be-83072204c44c"
+}
+# Define the resource group
+resource"azurerm_resource_group" "vsc" {
+  name  = "vsc"
+  location = "UKSouth"
+}
